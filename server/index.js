9a03445ed32e3974f925dd3d18caa4b315fd5685
amritansh-pandey <amritansh.p1010@gmail.com>
2022-05-29 21:02:45 +0530

    create backend function done

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://amritansh10:Amrit123@cluster0.a4a1w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://amrit1010:Amrit123@cluster0.zizl4.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
